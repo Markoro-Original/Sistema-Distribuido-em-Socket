2acc13fcf30b76dc3582f6f46c06d9c8ae86b6a0
Marcos Moraes <moraesvmarcos@gmail.com>
2023-07-10 14:42:42 -0300

    Teste de comunicação cliente-servidor.

---
+++ src/main/java/org/clienteServidor/ClientSocketExample.java
@@ -0,0 +1,45 @@
+package org.clienteServidor;
+ 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.lang.ClassNotFoundException;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+ 
+public class ClientSocketExample {
+    public static void main(String[] args) {
+        try {
+            //
+            // Create a connection to the server socket on the server application
+            //
+            InetAddress host = InetAddress.getLocalHost();
+            Socket socket = new Socket(host.getHostName(), 15000);
+ 
+            //
+            // Send a message to the client application
+            //
+            String message = "Hello there...";
+            ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
+            System.out.println("Cliente: " + message);
+            oos.writeObject(message);
+ 
+            //
+            // Read and display the response message sent by server application
+            //
+            ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
+            String resposta = (String) ois.readObject();
+            System.out.println("Servidor: " + resposta);
+ 
+            ois.close();
+            oos.close();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
